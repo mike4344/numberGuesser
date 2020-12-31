cf727212029a3e35ea5cc92153a5f87896bbb3d6
mike <mikemihalchik@gmail.com>
2020-12-30 19:15:56 -0500

    we were soooooo close

--- number-guesser.js
+++ number-guesser.js
@@ -6,24 +6,25 @@ let rl = readline.createInterface({
 });
 let min;
 let max;
-
+let secretNumber;
 let randomInRange = (min, max) =>{
   return Math.floor(Math.random()* (max - min)) + min;
 }
 
-let askRange = () =>{
+let askRange = (cb, cb2) =>{
 
     rl.question("enter a min number: ", answer =>{
-        min = answer
+        min = Number(answer)
         rl.question("enter a max number: ", answer2 =>{
-            max = answer2
-            rl.close()
+            max = Number(answer2)
+            secretNumber = cb2(min, max)
+            cb()
         })
     })
 }
 // Begin by initializing a variable in the global scope named secretNumber to any positive integer. Later we will program this variable to be assigned at random, but for now we'll hard-code a value that we can test for quickly.
 
-const secretNumber = randomInRange(min, max)
+
 
 /*
 Define a function named checkGuess that accepts a number as an argument. It should compare that argument against the global secretNumber. It should have the following behavior:
@@ -51,6 +52,7 @@ const checkGuess = num => {
 // Define a function named askGuess. The method should use the readline module's question method to ask the user to 'Enter a guess: '. If you need a refresher on how to use this method, check out the question docs. Once the user enters their number, the checkGuess function should be called with their number as an argument and the interface should be closed.
 
 const askGuess = () => {
+
   rl.question('Enter a guess: ', answer => {
     let lastAnswer = checkGuess(Number(answer));
     if(lastAnswer === false){
@@ -60,6 +62,7 @@ const askGuess = () => {
     }
   })
 }
-askRange()
-askGuess()
+
+askRange(askGuess,randomInRange)
+
 // When accepting user input, there is a very important nuance to take into account. When the user enters their guess it will be interpreted as a string of numeric characters and not an actual number type! Depending on how you wrote your checkGuess function, this could be disastrous because:
